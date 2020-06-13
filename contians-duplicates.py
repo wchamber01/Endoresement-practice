9942a262555cf7af3c421a054f59a9c65e056ab6
wchamber01 <williamchambers@gmail.com>
2020-06-12 23:01:44 -0400

    created contains-duplicates

---
+++ contians-duplicates.py
@@ -0,0 +1,5 @@
+# Your function should return true if any value appears at least twice in the array,
+# and it should return false if every element is distinct.
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
